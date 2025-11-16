8e6594c54a1e0727203a4b3c8d9fa43a9ebf5ad0
muzamilfaryad <muzamilfaryad77@gmail.com>
2025-11-17 00:35:53 +0500

    ci: add Netlify deployment configuration with Next.js plugin

--- next.config.js
+++ next.config.js
@@ -2,7 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+    ],
   },
 }
 
